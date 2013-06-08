ae96c8fabc71f181509afd59c411d50d5fb74db7
nham <hamann.nick@gmail.com>
2013-06-08 16:18:27 +0000

    add code for compiling notes/ section. also add code for creating a directory in the output folder if it doesnt already exist

--- compile.py
+++ compile.py
@@ -8,6 +8,8 @@ exclude_files = ['readme.md']
 out_folder = '_out/'
 
 
+fname_no_ext = lambda fn: fn[:fn.index('.')]
+
 def pandocConvert(pathto, fname):
     dothtml = fname[:fname.index('.')] + '.html'
     in_file = pathto + fname
@@ -31,6 +33,9 @@ def isPage(fname):
         and (fname[fname.index('.'):] in pages) \
         and fname not in exclude_files
 
+
+
+# Compilation script begins here
 for fname in os.listdir('./'):
     if isPage(fname):
         pandocConvert('', fname)
@@ -39,3 +44,11 @@ for fname in os.listdir('./'):
 for fname in os.listdir('./blov/'):
     if isPage(fname):
         pandocConvert('blov/', fname)
+
+for fname in os.listdir('./notes/'):
+    if isPage(fname) and fname_no_ext(fname) in ['linalg']:
+        out_dir = out_folder + 'notes/'
+        if not os.path.exists(out_dir):
+            os.makedirs(out_dir)
+
+        pandocConvert('notes/', fname)
