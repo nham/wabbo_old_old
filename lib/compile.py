ddde9e450c6dc173e416c4b6ef73d7bbc3db6815
nham <hamann.nick@gmail.com>
2013-07-07 16:19:30 +0000

    copy the google site verification file in driveler compile script

--- lib/compile.py
+++ lib/compile.py
@@ -7,6 +7,7 @@ d.css_file = '/css/style.css'
 
 
 d.copy_folder('css/')
+d.copy_file('google2f42c600d70282d6.html')
 d.convert_folder('', bl=['readme'])
 d.convert_folder('blov/')
 d.convert_folder('notes/', wl=['linalg', 'groups', 'logic'])
